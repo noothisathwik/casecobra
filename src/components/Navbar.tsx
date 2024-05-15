ead9170a98e4a782f1e9d87eeb30304a94bc3970
noothisathwik <noothisathwik26@gmail.com>
2024-05-15 13:45:24 +0000

    added full auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,12 @@ import MaxWidthWrapper from './MaxWidthWrapper'
 import Link from 'next/link'
 import { buttonVariants } from './ui/button';
 import { ArrowRight } from 'lucide-react';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
-const Navbar = () => {
-    const user = undefined;
-    const isAdmin = false;
+const Navbar =  async () => {
+    const{getUser} = getKindeServerSession();
+    const user = await getUser();
+    const isAdmin = user?.email===process.env.ADMIN_EMAIL;
   return (
     <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
         <MaxWidthWrapper >
@@ -33,7 +35,7 @@ const Navbar = () => {
                         <Link href="/api/auth/register" className={buttonVariants({size:'sm',variant:"ghost"})}>
                             Signup
                         </Link>
-                        <Link href="/api/login"className={buttonVariants({size:'sm',variant:"ghost"})}>
+                        <Link href="/api/auth/login"className={buttonVariants({size:'sm',variant:"ghost"})}>
                             Login
                         </Link>
                         <div className='h-8 w-px bg-zinc-200 sm:block'>
