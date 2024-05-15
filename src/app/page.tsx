bac1390dff49fb1ddabc3807aaa4189ac57ce1d8
noothisathwik <noothisathwik26@gmail.com>
2024-05-15 15:00:21 +0000

    done coding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { Check } from "lucide-react";
 import { Star } from "lucide-react";
 import Phone from "../components/Phone";
 import { Icons } from "@/components/Icons";
+import Reviews from "@/components/Reviews";
+
 
 export default function Home() {
   return (
@@ -123,9 +125,11 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            
           </div>
         </MaxWidthWrapper>
+        <div className="pt-16">
+          <Reviews/>
+        </div>
       </section>
     </div>
   );
